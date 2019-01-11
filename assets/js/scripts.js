9bc2eae06af79e10b158ed61b22142d499c543f6
Ryan <ryan0432@gmail.com>
2019-01-11 19:33:11 +1100

    Testing JS function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,9 @@ function hover(element)
     // skill icons hover
     if (element.id == "cpp-icon")
     {
-        element.src = "./assets/img/sk_Cpp_hover.svg";
+        element.style.transition = "src 2s";
+        console.log("fade");
+        //element.src = "./assets/img/sk_Cpp_hover.svg";
     }
     if (element.id == "csharp-icon")
     {
