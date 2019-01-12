803817fbfce848a4d8e4a7e1175292b085815b77
Ryan <ryan0432@gmail.com>
2019-01-13 00:10:10 +1100

    1. Made buttons and icons fade when hover by JS. 2. Aligned minor offsets in SVG icons.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,151 +1,30 @@
-function hover(element)
-{
-    // skill icons hover
-    if (element.id == "cpp-icon")
-    {
-        //element.style.transition = "src 2s";
-        console.log("fade");
-        element.src = "./assets/img/sk_Cpp_hover.svg";
-    }
-    if (element.id == "csharp-icon")
-    {
-        element.src = "./assets/img/sk_Csharp_hover.svg";
-    }
-    if (element.id == "html5-icon")
-    {
-        element.src = "./assets/img/sk_HTML5_hover.svg";
-    }
-    if (element.id == "css3-icon")
-    {
-        element.src = "./assets/img/sk_CSS3_hover.svg";
-    }
-    if (element.id == "js-icon")
-    {
-        element.src = "./assets/img/sk_JS_hover.svg";
-    }
-    if (element.id == "unity-icon")
-    {
-        element.src = "./assets/img/sk_Unity_hover.svg";
-    }
-    if (element.id == "maya-icon")
-    {
-        element.src = "./assets/img/sk_MAYA_hover.svg";
-    }
-    if (element.id == "ps-icon")
-    {
-        element.src = "./assets/img/sk_Photoshop_hover.svg";
-    }
-    if (element.id == "illu-icon")
-    {
-        element.src = "./assets/img/sk_Illustrator_hover.svg";
-    }
-    if (element.id == "ae-icon")
-    {
-        element.src = "./assets/img/sk_AfterEffects_hover.svg";
-    }
-    if (element.id == "ppro-icon")
-    {
-        element.src = "./assets/img/sk_PremierePro_hover.svg";
-    }
+function fadeIn(el, time) {
+    console.log("hover");
+    el.style.opacity = 0;
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+      last = +new Date();
+  
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      }
+    };
+  
+    tick();
+  }
 
-    // sns icons hover
-    if (element.id == "email-icon")
-    {
-        element.src = "./assets/img/sns_Email_hover.svg";
-    }
-    if (element.id == "github-icon")
-    {
-        element.src = "./assets/img/sns_Github_hover.svg";
-    }
-    if (element.id == "linkedin-icon")
-    {
-        element.src = "./assets/img/sns_Linkedin_hover.svg";
-    }
-    if (element.id == "tumblr-icon")
-    {
-        element.src = "./assets/img/sns_Tumblr_hover.svg";
-    }
-    if (element.id== "twitter-icon")
-    {
-        element.src = "./assets/img/sns_Twitter_hover.svg";
-    }
-    if (element.id == "vimeo-icon")
-    {
-        element.src = "./assets/img/sns_Vimeo_hover.svg";
-    }
-}
-
-function unhover(element)
-{
-    // skill icons default
-    if (element.id == "cpp-icon")
-    {
-        element.src = "./assets/img/sk_Cpp_default.svg";
-    }
-    if (element.id == "csharp-icon")
-    {
-        element.src = "./assets/img/sk_Csharp_default.svg";
-    }
-    if (element.id == "html5-icon")
-    {
-        element.src = "./assets/img/sk_HTML5_default.svg";
-    }
-    if (element.id == "css3-icon")
-    {
-        element.src = "./assets/img/sk_CSS3_default.svg";
-    }
-    if (element.id == "js-icon")
-    {
-        element.src = "./assets/img/sk_JS_default.svg";
-    }
-    if (element.id == "unity-icon")
-    {
-        element.src = "./assets/img/sk_Unity_default.svg";
-    }
-    if (element.id == "maya-icon")
-    {
-        element.src = "./assets/img/sk_MAYA_default.svg";
-    }
-    if (element.id == "ps-icon")
-    {
-        element.src = "./assets/img/sk_Photoshop_default.svg";
-    }
-    if (element.id == "illu-icon")
-    {
-        element.src = "./assets/img/sk_Illustrator_default.svg";
-    }
-    if (element.id == "ae-icon")
-    {
-        element.src = "./assets/img/sk_AfterEffects_default.svg";
-    }
-    if (element.id == "ppro-icon")
-    {
-        element.src = "./assets/img/sk_PremierePro_default.svg";
-    }
-
-    // sns icons default
-    if (element.id == "email-icon")
-    {
-        element.src = "./assets/img/sns_Email_default.svg";
-    }
-    if (element.id == "github-icon")
-    {
-        element.src = "./assets/img/sns_Github_default.svg";
-    }
-    if (element.id == "linkedin-icon")
-    {
-        element.src = "./assets/img/sns_Linkedin_default.svg";
-    }
-    if (element.id == "tumblr-icon")
-    {
-        element.src = "./assets/img/sns_Tumblr_default.svg";
-    }
-    if (element.id == "twitter-icon")
-    {
-        element.src = "./assets/img/sns_Twitter_default.svg";
-    }
-    if (element.id == "vimeo-icon")
-    {
-        element.src = "./assets/img/sns_Vimeo_default.svg";
-    }
-}
+  function fadeOut(el, time) {
+    el.style.opacity = 1;
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity - (new Date() - last) / time;
+      last = +new Date();
+  
+      if (+el.style.opacity > 0) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+      }
+    };
+  
+    tick();
+  }
