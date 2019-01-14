c7ca0fda5d453e8b0eab26bd82a31cb1fbbc6bd3
Ryan <ryan0432@gmail.com>
2019-01-15 01:37:21 +1100

    Addded animated dropdown menu.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,33 @@
-function fadeIn(el, time) {
+////////////////////////
+//! Global Variables !//
+////////////////////////
+
+// Get computed value
+const navBarHeight_String = getComputedStyle(document.documentElement, null).getPropertyValue('--nav-bar-height');
+
+// Get only number from value
+var navBarHeight_Num = navBarHeight_String.replace(/[^0-9\.]+/g, '');
+
+// Convert string to number
+navBarHeight_Num = Number(navBarHeight_Num);
+
+// Define suffix
+const unitSuffix = 'rem';
+
+// Array of buttons
+const navButtonList = document.getElementsByClassName("dropdown-content");
+
+// Validate if  Nav bar is opened
+var isNavbarOpened = false;
+
+
+////////////////////////
+//! Public Functions !//
+////////////////////////
+
+// Fade In
+function fadeIn(el, time)
+{
     console.log("hover");
     el.style.opacity = 0;
     var last = +new Date();
@@ -13,8 +42,9 @@ function fadeIn(el, time) {
   
     tick();
   }
-
-  function fadeOut(el, time) {
+  // Fade Out
+  function fadeOut(el, time)
+  {
     el.style.opacity = 1;
     var last = +new Date();
     var tick = function() {
@@ -27,4 +57,41 @@ function fadeIn(el, time) {
     };
   
     tick();
+  }
+
+  // Open Dropdown Menu
+  function openDropdown()
+  {
+    if(!isNavbarOpened)
+    {
+      isNavbarOpened = true;
+      
+      let navBarHeight_NumToString = `${navBarHeight_Num * navButtonList.length}${unitSuffix}`;
+
+      document.getElementById("dropdownNav").style.height = navBarHeight_NumToString;
+      document.getElementById("dropdownNav").style.pointerEvents = "auto";
+
+      for(var i = 0; i < navButtonList.length; ++i)
+      {
+        navButtonList[i].style.opacity = "1";
+      }
+    }
+    else
+    {
+      closeDropdown();
+    }
+  }
+  
+  // Close Dropdown Menu
+  function closeDropdown()
+  {
+    isNavbarOpened = false;
+
+    document.getElementById("dropdownNav").style.height = "0%";
+    document.getElementById("dropdownNav").style.pointerEvents = "none";
+    
+    for(var i = 0; i < navButtonList.length; ++i)
+    {
+      navButtonList[i].style.opacity = "0";
+    }
   }
